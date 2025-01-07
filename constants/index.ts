d8d44349ef767a8e4fbc7d981f971aec4a9aafcd
abinashbolt <abinashbolt@gmail.com>
2025-01-07 14:54:43 +0530

    initial commit

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_hilink_work', label: 'How Abinash Travels Work?' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION 
+  export const FEATURES = [
+    {
+      title: 'Real maps can be offline',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'We provide a solution for you to be able to use our application when travelling, yes offline mode you can use at any time there is no signal at the location',
+    },
+    {
+      title: 'Set an adventure schedule',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "Schedule an adventure with friends. On holidays, there are many interesting offers from Abinash Travels. That way, there's no more discussion",
+    },
+    {
+      title: 'More Complementary',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'we are giving more complementary like 24*7 water service,some biscuits and we will give you a beadsheet for thr full day you travel',
+    },
+    {
+      title: 'Many new locations every month',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'Lots of new locations every month, because we have a worldwide community of travels who share their best experiences with travelling',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About Abinash Travels',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Travelling xixixi', 'Booking links', 'Abinash chennai'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'abinash@travels.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
